6332e00cb184300bb3ce3359ce43176541d16837
Renata Happle <renatahapp@gmail.com>
2018-07-26 20:16:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+
+var driver = {}
+
+// var sam.driver = {
+//   address: "11 Broadway",
+//   name: "Sam",
+// }
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  let result = 
+  return result 
+}
+
+var sam = { 
+  address: "11 Broadway",
+  
